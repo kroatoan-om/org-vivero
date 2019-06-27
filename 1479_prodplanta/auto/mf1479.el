04a644f0e6b7b4d637f130da29d6e6cdbae28c92
asoler <mescaliyo@gmail.com>
2019-06-27 14:44:20 +0200

    cambiado a asgbook class y compilado.

--- 1479_prodplanta/auto/mf1479.el
+++ 1479_prodplanta/auto/mf1479.el
@@ -2,20 +2,20 @@
  "mf1479"
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
-                     '(("article" "a4paper" "12pt" "oneside")))
+                     '(("book" "a4paper" "12pt" "oneside")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("babel" "main=spanish" "english" "") ("inputenx" "utf8") ("fontenc" "T1") ("color" "dvipsnames") ("inputenc" "utf8") ("ulem" "normalem")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
-    "article"
-    "art12"
+    "book"
+    "bk12"
     "babel"
     "inputenx"
     "fontenc"
@@ -40,14 +40,14 @@
     "ulem"
     "capt-of")
    (LaTeX-add-labels
-    "sec:orgaf7fcae"
-    "sec:org2f98878"
-    "sec:orgff178b2"
-    "sec:org35e0851"
-    "sec:orge5d2e27"
-    "sec:orga7c6b58"
-    "sec:orgdd7ccda"
-    "sec:org0105c8e"
-    "sec:org241d138"))
+    "sec:org8448095"
+    "sec:org64e2d83"
+    "sec:org7d2b90f"
+    "sec:org47fb70b"
+    "sec:org112b6a0"
+    "sec:org925efae"
+    "sec:orga898aa8"
+    "sec:orge936ade"
+    "sec:org7b0fee1"))
  :latex)
 
